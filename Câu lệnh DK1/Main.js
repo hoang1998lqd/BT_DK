ee15234b4abe84367ee7342dd958c7ad9750ef53
V Regalite <hoang1998lqd@gmail.com>
2022-05-14 17:24:08 +0700

    Add file bài tập

---
+++ Câu lệnh DK1/Main.js
@@ -0,0 +1,52 @@
+// function check() {
+//     let number1 =document.getElementById('soA');
+//     let number2 =document.getElementById('soB');
+//         let a = parseInt(number1);
+//         let b = parseInt(number2);
+//     if (a % b === 0)
+//         document.getElementById('chiahet').innerHTML = "a chia hết cho b";
+//
+//     else
+//         document.getElementById('chiahet').innerHTML = "a không chia hết cho b"
+//
+//
+// }
+
+// Bài 1
+// let a = +prompt("Nhập a");
+// let b = +prompt("Nhập b");
+// if (a % b === 0) {
+//     alert("a chia hết cho b");
+// } else {
+//     alert("a không chia hết cho b");
+// }
+
+// Bài 2
+// let age = +prompt("Nhập tuoi");
+// console.log(age)
+// if (age >= 16) {
+//     alert("Bạn đã đủ tuổi lên lớp");
+// } else {
+//     alert("Bạn không đủ tuổi lên lớp");
+// }
+
+
+// Bài 4
+// function timMax() {
+//     let a = parseInt(document.getElementById('soA').value);
+//     let b = parseInt(document.getElementById('soB').value);
+//     let c = parseInt(document.getElementById('soC').value);
+//     if (a>b && b> c){
+//         document.getElementById('solon').innerText = "Số lớn nhất là " + a
+//     }
+//         else if (b>a && b> c){
+//             document.getElementById('solon').innerText= "Số lớn nhất là " + b
+//     }
+//         else{
+//             document.getElementById("solon").innerText= 'Số lớn nhất là ' + c
+//         }
+// }
+
+
+// Bài 5: Xếp hạng học lực
+
