24886d555eb9b058c0e2ab8d2f15517e97e5586a
V Regalite <hoang1998lqd@gmail.com>
2022-05-14 21:55:43 +0700

    Update

--- Câu lệnh DK1/Main.js
+++ Câu lệnh DK1/Main.js
@@ -46,7 +46,59 @@
 //             document.getElementById("solon").innerText= 'Số lớn nhất là ' + c
 //         }
 // }
+//
+
+///////////////////////////
+// Luyện tập cấu trúc câu điều kiện 2
+
+// Bài 1:
+
+// Bài 7: Giải phương trình bậc 2
+// function timN() {
+//     let a = parseInt(document.getElementById('nhapa').value);
+//     let b = parseInt(document.getElementById('nhapb').value);
+//     let c = parseInt(document.getElementById('nhapc').value);
+//     let e;
+//     let x1;
+//     let x2;
+//
+//     if ( a===0 && b === 0 && c=== 0){
+//         document.getElementById('ptbac2').innerText = "Phương trình vô số nghiệm"
+//     }
+//     else if (a===0 && b === 0 && c !== 0){
+//         document.getElementById('ptbac2').innerText = "Phương trình vô nghiệm"
+//
+//     }
+//     else if (a===0 && b!==0 && c!==0){
+//         let d = (-c)/b
+//         document.getElementById('ptbac2').innerText = "Phương trình có nghiệm duy nhất x = " + d;
+//     }
+//     else{
+//        e = (b**2)-4*a*c
+//         if (e < 0){
+//             document.getElementById('ptbac2').innerText = "Phương trình vô nghiệm"
+//         }
+//         else if (e === 0){
+//            x1 =  (-b)/(2*a);
+//            document.getElementById('ptbac2').innerText = "Phương trình có nghiệm duy nhất x = " + x1;
+//         }
+//         else
+//             x1 = ((-b)+Math.sqrt(e))/(2*a);
+//             x2 = ((-b)-Math.sqrt(e))/(2*a);
+//             document.getElementById('ptbac2').innerHTML = "Phương trình có 2 nghiệm phân biệt: " + "Nghiệm x1 = " + x1.toFixed(3) + "Nghiệm x2 = " + x2.toFixed(3)
+//
+//     }
+//
+//
+//
+//
+// }
 
 
-// Bài 5: Xếp hạng học lực
+
+//
+
+
+
+
 
